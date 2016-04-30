d2387ad152e8ec6ddfe987f59dd7c0013e51f79f
meshnesh <tonnie.nesh@gmail.com>
2016-04-30 22:13:55 +0300

    #10

---
+++ frequency.py
@@ -0,0 +1,6 @@
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim():
+    location = geolocator.geocode("175 5th Avenue NYC")
+        print(location.address)
+        print((location.latitude, location.longitude))
